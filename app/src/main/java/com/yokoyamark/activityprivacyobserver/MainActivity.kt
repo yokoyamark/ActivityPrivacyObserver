4a323ae65468d3510245eecc4c703070284e77f8
yokoyamark <indexbraker02@yahoo.co.jp>
2021-05-05 22:13:53 +0900

    add lib code

--- app/src/main/java/com/yokoyamark/activityprivacyobserver/MainActivity.kt
+++ app/src/main/java/com/yokoyamark/activityprivacyobserver/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.yokoyamark.activityprivacyobserver
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yokoyamark.privacyobserver.PrivacyObserver
 
 class MainActivity : AppCompatActivity() {
+    lateinit var privacyObserver: PrivacyObserver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        privacyObserver = PrivacyObserver(this, R.layout.activity_splash)
+        lifecycle.addObserver(privacyObserver)
     }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        privacyObserver.onUserLeaveHint()
+    }
+
 }
---
+++ app/src/main/java/com/yokoyamark/activityprivacyobserver/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.yokoyamark.activityprivacyobserver
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        window.insetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+        window.insetsController?.systemBarsBehavior =
+            WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        GlobalScope.launch(Dispatchers.Default) {
+            delay(1000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }
+    }
+}
